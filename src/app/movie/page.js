5322e0fdae47a0d88d7169a9cae54f680323a613
rajparmar4462 <115870589+rajparmar4462@users.noreply.github.com>
2023-07-06 10:18:29 +0530

    api-commit1

---
+++ src/app/movie/page.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import MovieCard from "@/app/components/MovieCard";
+import styles from "@/app/styles/common.module.css"
+
+const Movie = async () => {
+
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+
+    const url = process.env.RAPID_KEY;
+
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "e53f0ed85amsh36329b7bc417244p1cd2a4jsn1995e44caa03",
+        "X-RapidAPI-Host": "netflix54.p.rapidapi.com",
+      },
+    };
+
+    const res = await fetch(url, options);
+    const data = await res.json();
+    const main_data = data.titles;
+    console.log(main_data.jawSummary)
+
+    return (
+        <>
+            <section className={styles.movieSection}>
+                <div className={styles.container}>
+                    <h1>Series & Movie</h1>
+                    <div className={styles.card_section}>
+                        {
+                            main_data.map((curElem) => {
+                                return <MovieCard key={curElem.id} {...curElem} />
+                            })
+                        }
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+};
+
+export default Movie;
